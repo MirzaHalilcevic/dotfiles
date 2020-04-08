ceb590ac924f634a6e0b505029c21db2417ce7d6
Mirza Halilcevic <mirza.halilcevic@marvelsoft.net>
2020-04-09 01:03:29 +0200

    stow support
    Signed-off-by: Mirza Halilcevic <mirza.halilcevic@marvelsoft.net>

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-ln -s $HOME/dotfiles/zshrc             $HOME/.zshrc
-ln -s $HOME/dotfiles/p10k.zsh          $HOME/.p10k.zsh
-ln -s $HOME/dotfiles/init.vim          $HOME/.config/nvim/init.vim
-ln -s $HOME/dotfiles/coc-settings.json $HOME/.config/nvim/coc-settings.json
-ln -s $HOME/dotfiles/kitty.conf        $HOME/.config/kitty/kitty.conf
+stow kitty zsh nvim
